dd2318d5fa3cbcd5f2de914882419db80242e0da
danal8 <giorgosdanalatos@gmail.com>
2026-07-21 14:41:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
+var CACHE = 'mediprice-v2';
 self.addEventListener('install', function(e){ self.skipWaiting(); });
 self.addEventListener('activate', function(e){ e.waitUntil(clients.claim()); });
 self.addEventListener('fetch', function(e){ e.respondWith(fetch(e.request)); });
+self.addEventListener('message', function(e){
+  if(e.data && e.data.action==='skipWaiting') self.skipWaiting();
+});
